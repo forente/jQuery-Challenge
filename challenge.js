d855d36207f560bb4ee97e7f45c701be36bc461a
forente <zerofox16@gmail.com>
2016-06-30 15:57:25 -0500

    added button change

--- challenge.js
+++ challenge.js
@@ -4,8 +4,21 @@ $(document).ready(function(){
   for (var i = 1; i <= numOfButtons; i++) {
     $('body').append('<button value='+i+'>'+ i+'</button>');
   }
+  $('body').append("<div></div>");
 
-  
+var isStored = false;
+var buttonVal =0;
+  $('button').on('click', function(){
+
+    if(isStored){
+      $('div').append(buttonVal +" + "+$(this).val()+"=" (buttonVal + $(this).val()) );
+      isStored = !isStored;
+    }
+    else{
+      buttonVal =  $(this).val();
+    }
+
+  });
 
 
 });
